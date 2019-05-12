4bce9b3ec465803e4a6c4828c33c37c2d9efb955
Karl Pokus <karl.pokus@gmail.com>
2019-05-12 16:33:19 +0200

    add comment on expected event op

--- internal/mockw/mockw.go
+++ internal/mockw/mockw.go
@@ -16,12 +16,12 @@ func (m *Mock) Walk(root string, t tree.Tree) error {
 	defer func() { m.i++ }()
 	if m.i == 0 { // base
 		t["a"] = int64(1)
-		t["b"] = int64(1)
+		t["b"] = int64(1) // removed
 		return nil
 	}
 	if m.i == 1 { // fs
-		t["a"] = int64(2)
-		t["c"] = int64(1)
+		t["a"] = int64(2) // updated
+		t["c"] = int64(1) // created
 		return nil
 	}
 	return PathErr
