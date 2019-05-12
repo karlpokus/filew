48d9f5670eec1818c7bed2e4cd8cfe53b1e73d04
Karl Pokus <karl.pokus@gmail.com>
2019-05-12 12:48:25 +0200

    add mem usage output flag

--- cmd/filew/main.go
+++ cmd/filew/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
   "fmt"
+  "flag"
+  "runtime"
+  "time"
+  "os"
+
   "github.com/karlpokus/filew"
 )
 
+var mem = flag.Bool("mem", false, "toggle mem usage output")
+
 func main() {
-  fpath := "testdata/small"
+  flag.Parse()
+  if *mem {
+    go memUsage()
+  }
+  fpath := "testdata/express"
   events, err := filew.Watch(fpath, nil)
   if err != nil {
     panic(err)
@@ -15,4 +26,17 @@ func main() {
   for ev := range events {
     fmt.Println(ev)
   }
+}
+
+func memUsage() {
+  for {
+    var m runtime.MemStats
+    runtime.ReadMemStats(&m)
+    fmt.Printf("memUsage: heap=%vMB, sys=%vMB\n", bToMb(m.Alloc), bToMb(m.Sys))
+    time.Sleep(10 * time.Second)
+  }
+}
+
+func bToMb(b uint64) uint64 {
+  return b / 1024 / 1024
 }
